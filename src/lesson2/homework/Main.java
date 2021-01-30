ff4e4c99a58c24e574bd0dff57bfedb79adc859f
toygildinpv <protagonyst@mail.ru>
2021-01-30 21:39:54 +0400

    homewrok 2 done

--- src/lesson2/homework/Main.java
+++ src/lesson2/homework/Main.java
@@ -1,9 +1,13 @@
 package lesson2.homework;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        String[][] arr1 = {{"1", "2", "3", "4"}, {"-1", "-2", "-3", "-4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}};
-        String[][] arr2 = {{"1", "s", "3", "4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}};
+        String[][] arr1 = {{"1", "2", "3", "4"}, {"-1", "-2", "-3", "-4"},
+                {"1", "2", "3", "4"}, {"1", "2", "3", "4"}};
+        String[][] arr2 = {{"1", "2", "3", "4"}, {"1", "2", "3", "sd"},
+                {"1", "2", "3", "4"}, {"1", "2", "3", "4"}};
 
         String[][] arr3 = new String[4][];
         for (int i = 0; i < arr3.length; i++) {
@@ -12,11 +16,21 @@ public class Main {
 
         String[][] arr4 = new String[2][5];
 
+        fillArray(arr3);
+        fillArray(arr4);
+
         System.out.println("Массив arr1:");
+        showArray(arr1);
         sumArray(arr1);
         System.out.println("Массив arr2:");
+        showArray(arr2);
         sumArray(arr2);
-        //sumArray(arr3);
+        System.out.println("Массив arr3:");
+        showArray(arr3);
+        sumArray(arr3);
+        System.out.println("Массив arr4:");
+        showArray(arr4);
+        sumArray(arr4);
 
     }
 
@@ -46,11 +60,29 @@ public class Main {
                     sum += number;
                 } catch (NumberFormatException exception) {
                     throw new MyArrayDataException(String.format(
-                            "Данные в ячейке (%s; %s) невозможно преобразовать в числовой формат", i, j));
+                            "Данные в ячейке (строка %s; столбец %s) невозможно преобразовать в числовой формат",
+                            i + 1, j + 1));
                 }
             }
         }
-        System.out.printf("\nСумма всех членов массива = %s\n", sum);
+        System.out.printf("Сумма всех членов массива = %s\n", sum);
     }
 
+    public static void fillArray (String[][] array) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = String.valueOf(random.nextInt(10));
+            }
+        }
+    }
+
+    public static void showArray (String[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
