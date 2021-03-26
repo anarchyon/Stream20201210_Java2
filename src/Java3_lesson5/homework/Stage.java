4caee1a3a9bae434c19575712160b66a0e5ccc1a
toygildinpv <protagonyst@mail.ru>
2021-03-26 19:19:29 +0400

    classes copied from task

---
+++ src/Java3_lesson5/homework/Stage.java
@@ -0,0 +1,12 @@
+package Java3_lesson5.homework;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract void go(Car c);
+}
